e3ec77114e5de74644a50a708f743aa226c0d6fb
NastyaSkripka <skripka_nastena@bk.ru>
2022-10-26 23:56:05 +0300

    MEOWSELL

---
+++ src/main/java/com/example/meowsell/MeowsellApplication.java
@@ -0,0 +1,13 @@
+package com.example.meowsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowsellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowsellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meowsell/MeowsellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meowsell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowsellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
